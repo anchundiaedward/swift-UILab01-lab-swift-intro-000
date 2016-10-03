e7a51f04a657382f18e8b31b61b7e50184537f14
edwardanchundia <edward.anchundia@gmail.com>
2016-10-03 12:18:49 -0400

    completed uilabel lab

--- LightBulb/ViewController.swift
+++ LightBulb/ViewController.swift
@@ -17,14 +17,30 @@ class ViewController: UIViewController {
         
         super.viewDidLoad()
         
-        lightBulb.backgroundColor = UIColor.blue
+        lightBulb.backgroundColor = UIColor.white
         
     }
     
     
     @IBAction func colorSelected(_ sender: UISegmentedControl) {
         
-        print(sender.selectedSegmentIndex)
+        let index = sender.selectedSegmentIndex
+        
+//        print(sender.selectedSegmentIndex)
+        
+        switch index {
+        case 0:
+            lightBulb.backgroundColor = UIColor.red
+        case 1:
+            lightBulb.backgroundColor = UIColor.yellow
+        case 2:
+            lightBulb.backgroundColor = UIColor.blue
+        case 3:
+            lightBulb.backgroundColor = UIColor.green
+        default:
+            break
+        }
+
         
     }
     
